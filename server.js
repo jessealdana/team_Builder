09d0acc52745f01dd04336bba1004cd84f2e09e8
Jesse Aldana <jesse.aldana@gmail.com>
2020-05-28 19:55:43 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,199 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+require("console.table");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "D0))Ph1Np0d",
+  database: "employee_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  chooseAct();
+});
+
+function chooseAct() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: [
+        "Add department",
+        "Add role",
+        "Add employee",
+        "View department",
+        "View role",
+        "View employee",
+        "Update employee role"
+      ]
+    })
+    .then(function(answer) {
+
+      switch (answer.action) {
+      case "Add department":
+        insertDept();
+        break;
+
+      case "Add role":
+        insertRole();
+        break;
+
+      case "Add employee":
+        insertEmployee();
+        break;
+
+      case "View department":
+        deptSearch();
+        break;
+
+      case "View role":
+        roleSearch();
+        break;
+
+      case "View employee":
+        employeeSearch();
+        break;
+
+      case "Update employee role":
+        updateRole();
+        break;
+      }
+    });
+}
+
+function insertDept() {
+    inquirer
+      .prompt([
+        {
+          name: "deptName",
+          type: "input",
+          message: "What is the name of the department you would like to add?"
+        }
+      ])
+      .then(function(answer) {
+        connection.query(
+          "INSERT INTO department SET ?",
+          {
+            deptName: answer.deptName,
+          },
+          function(err) {
+            if (err) throw err;
+            console.log("Your department was created successfully!");
+            chooseAct();
+          }
+        );
+      });
+  };
+  
+  function insertRole() {
+    inquirer
+      .prompt([
+        {
+          name: "title",
+          type: "input",
+          message: "What is the title of the employee role you would like to add?"
+        },
+        {
+          name: "salary",
+          type: "input",
+          message: "What is the salary of the employee role you would like to add?"
+        },
+        {
+          name: "dept_id",
+          type: "input",
+          message: "What is the id of the department in which this role will be filed?"
+        }
+      ])
+      .then(function(answer) {
+        connection.query(
+          "INSERT INTO employeeRole SET ?",
+          {
+            title: answer.title,
+            salary: answer.salary,
+            dept_id: answer.dept_id
+          },
+          function(err) {
+            if (err) throw err;
+            console.log("Your employee role was created successfully!");
+            chooseAct();
+          }
+        );
+      });
+  }
+  
+  function insertEmployee() {
+    inquirer
+      .prompt([
+        {
+          name: "first_name",
+          type: "input",
+          message: "What is the first name of the new employee?"
+        },
+        {
+          name: "last_name",
+          type: "input",
+          message: "What is the last name of the new employee"
+        },
+        {
+          name: "dept_id",
+          type: "input",
+          message: "What is the department id of the new employee?"
+        }
+      ])
+      .then(function(answer) {
+        connection.query(
+          "INSERT INTO employee SET ?",
+          {
+            first_name: answer.first_name,
+            last_name: answer.last_name,
+            dept_id: answer.dept_id
+          },
+          function(err) {
+            if (err) throw err;
+            console.log("Your employee was created successfully!");
+            chooseAct();
+          }
+        );
+      });
+  }
+
+  function deptSearch() {
+          connection.query("SELECT * FROM department;", function(err, res) {
+            if (err) throw err;
+            console.log("\n");
+            console.table(res);
+
+          });
+          chooseAct();
+        };
+
+  function roleSearch() {
+        connection.query("SELECT * FROM employeeRole;", function(err, res) {
+          if (err) throw err;
+          console.log("\n");
+          console.table(res);
+        });
+        chooseAct();
+  };
+
+  function employeeSearch() {
+    connection.query("SELECT * FROM employee;", function(err, res) {
+      if (err) throw err;
+      console.log("\n");
+      console.table(res);
+    });
+    chooseAct();
+};
+
+
+    
